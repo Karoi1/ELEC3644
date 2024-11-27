dfd0c9699ac0158c79c421531a2c9c802c92eefb
Karoi1 <66458732+Karoi1@users.noreply.github.com>
2024-11-27 16:53:45 +0800

    Update RecipeListView.swift

--- RecipeAppV1/Recipe App/RecipeListView.swift
+++ RecipeAppV1/Recipe App/RecipeListView.swift
@@ -11,35 +11,31 @@ struct MySwiftUIApp: App {
 
 struct RecipeListView: View {
     @StateObject var viewModel = RecipeViewModel()
-    public var Alllist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    var recipeID: [Int]
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                let columns = [
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ]
+        ScrollView {
+            let columns = [
+                GridItem(.flexible()),
+                GridItem(.flexible())
+            ]
 
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(viewModel.search(ListID: Alllist)) { recipe in
-                        NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
-                            RecipeRowView(recipe: recipe)
-                        }
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(viewModel.search(ListID: recipeID)) { recipe in
+                    NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                        RecipeRowView(recipe: recipe)
                     }
                 }
-                .padding()
             }
-            .navigationTitle("All Recipes")
-            .onAppear {
-                viewModel.loadRecipes()
-                print("onAppear called")
-            }
+            .padding()
+        }
+        .onAppear {
+            viewModel.loadRecipes()
         }
     }
 }
 
 
 #Preview {
-    RecipeListView()
+    RecipeListView(recipeID: [1,2,3,4,5,6,7,8,9,10,11])
 }
